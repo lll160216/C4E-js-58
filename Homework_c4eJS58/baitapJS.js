b1bf6c8505d732484fe5269c865202dec502ce13
Very Long <lll160216@gmail.com>
2019-11-12 15:33:11 +0700

    up

---
+++ Homework_c4eJS58/baitapJS.js
@@ -0,0 +1,7 @@
+let cauhoiten = prompt (" ban ten la gi ");
+console.log("chao " + cauhoiten);
+let chieudai = prompt("Chieu dai HCN là ");
+let chieurong = prompt ("Chieu rong HCN la ");
+console.log("chu vi HCN la "+(chieudai*2 + chieurong*2));
+console.log("dien tich HCN la "+ (chieudai*chieurong));
+

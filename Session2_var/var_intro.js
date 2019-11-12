b4e31ad0030c713f2dad4d7a527e41bc4bb1ad8d
Very Long <lll160216@gmail.com>
2019-11-12 14:36:47 +0700

    update lai bai tap

---
+++ Session2_var/var_intro.js
@@ -0,0 +1,22 @@
+alert ("Hello World, Hello MindX c4e");
+let name;
+var name1;
+const name2 = "Very Long";// const phai khoi tao luon
+name = "Long Pro";
+name1 = " LongLong";
+// alert(name);
+let x = 10;
+let y = 0;
+console.log(x);
+console.log(y);
+console.log(x+y);
+let z;
+z = (x + y)* (x + y);
+
+console.log(z);
+let tuoi=prompt("ban sinh nam bao nhieu ");
+console.log("tuoi cua ban la " + (2019 - tuoi))
+let ten;
+ten = prompt(" Ten ban la ");
+console.log("xin chao "+ ten);
+

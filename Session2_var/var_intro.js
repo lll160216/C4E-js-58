81b12cb8f60a4937b7e6de2c6d55418be2f6b955
Very Long <lll160216@gmail.com>
2019-11-14 21:20:01 +0700

    session 3

--- Session2_var/var_intro.js
+++ Session2_var/var_intro.js
@@ -1,22 +1,33 @@
-alert ("Hello World, Hello MindX c4e");
-let name;
-var name1;
-const name2 = "Very Long";// const phai khoi tao luon
-name = "Long Pro";
-name1 = " LongLong";
-// alert(name);
-let x = 10;
-let y = 0;
-console.log(x);
-console.log(y);
-console.log(x+y);
-let z;
-z = (x + y)* (x + y);
+// alert ("Hello World, Hello MindX c4e");
+// let name;
+// var name1;
+// const name2 = "Very Long";// const phai khoi tao luon
+// name = "Long Pro";
+// name1 = " LongLong";
+// // alert(name);
+// let x = 10;
+// let y = 0;
+// console.log(x);
+// console.log(y);
+// console.log(x+y);
+// let z;
+// z = (x + y)* (x + y);
 
-console.log(z);
-let tuoi=prompt("ban sinh nam bao nhieu ");
-console.log("tuoi cua ban la " + (2019 - tuoi))
-let ten;
-ten = prompt(" Ten ban la ");
-console.log("xin chao "+ ten);
+// console.log(z);
+// let tuoi=prompt("ban sinh nam bao nhieu ");
+// console.log("tuoi cua ban la " + (2019 - tuoi))
+// let ten;
+// ten = prompt(" Ten ban la ");
+// console.log("xin chao "+ ten);
+// let ketqua;
+// let a;
+// // console.log(a);
+// a = prompt ("nhap a");
 
+// ketqua =(4*(a*a+10*a*Math.sqrt(a)+3*a));
+// console.log("ket qua cua bieu thuc la "+ ketqua);
+{
+    // let a = 20;
+    var a = 20;
+}
+console.log(a);
---
+++ Session3_datatype/datatype.js
@@ -0,0 +1,45 @@
+// let name = `Long`;
+// let age = 19;
+// console.log(`Hello ${name}!`);
+// let x=20;
+// let y=30;
+// console.log(x+y);
+// // let z = x + y;
+// // console.log(z);
+// // console.log(`${x} + ${y} = ${x+y}`);
+// // console.log(`${x} - ${y} = ${x-y}`);
+// // console.log(`${x} * ${y} = ${x*y}`);
+// // console.log(`${x} / ${y} = ${x/y}`);
+// // console.log(`${x} % ${y} = ${x%y}`);
+// // console.log(x);
+// // ************************/
+
+
+// // console.log(x+y);
+// // console.log(`x++ ${x++}`);
+// // console.log(`y++ ${y++}`);
+// // console.log(`${--x}`);
+// // console.log(`${++y}`);
+
+// //***************************/
+
+// let a = "20";
+// let b ='30';
+// console.log(a+b);
+// console.log(a-b);
+// console.log(a/b);
+// console.log(a*b);
+// //chuyển b về 1 số
+// b = Number(b);
+// //Chuyển a về số nguyên 
+// a = parseInt(a);
+// //Chuyển a về số thực
+// a = parseInt(a)
+ 
+let name;
+let year;
+name = prompt("Nhập tên của bạn ");
+console.log(`Chào ${name}`);
+year = parseInt(prompt("Bạn sinh năm bao nhiêu ??"));
+console.log(`Tuổi của bạn là  ${2019-year}`);
+
